3b3a6cf4bc911e8805c2d3825746675b9d3b91a0
Jhoss Naranjo <djosephn@gmail.com>
2021-04-21 12:20:54 -0500

    Orbit Control added

--- js/default.js
+++ js/default.js
@@ -1,9 +1,11 @@
 import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
+import { OrbitControls } from 'https://unpkg.com/three@0.127.0/examples/jsm/controls/OrbitControls.js';
 
 console.log("ThreeJS funciona: ", THREE);
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.z = 3;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -14,15 +16,14 @@ const material = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe: true
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.z = 3;
-
-function animate() {
-	requestAnimationFrame( animate );
-
+const animate = () => {
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
-
-	renderer.render( scene, camera );
+	renderer.render(scene, camera);
+	requestAnimationFrame(animate); // Animate loop
 }
 
 animate();
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.update();
