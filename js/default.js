971319404823f38ea9dad67999af49749f1b3daf
Jhoss Naranjo <djosephn@gmail.com>
2021-04-21 13:32:03 -0500

    Add gltf model to the scene

--- js/default.js
+++ js/default.js
@@ -1,29 +1,42 @@
 import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
+import { GLTFLoader } from 'https://unpkg.com/three@0.127.0/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from 'https://unpkg.com/three@0.127.0/examples/jsm/controls/OrbitControls.js';
 
 console.log("ThreeJS funciona: ", THREE);
+console.log("GLTFLoader funciona: ", GLTFLoader);
+console.log("OrbitControls funciona: ", OrbitControls);
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.z = 3;
+camera.position.z = 4;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe: true } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.update();
 
-const animate = () => {
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+const loader = new GLTFLoader();
+
+loader.load(
+	'model/world.gltf',
+	gltf => {
+		scene.add(gltf.scene);
+	}
+);
+
+// Create a light
+const light = new THREE.AmbientLight("white"); // soft white light
+scene.add( light );
+
+
+/** Create a loop that causes the renderer to draw the scene every time the screen is refreshed
+* @param { number } time - A timestamp number returned by requestAnimationFrame()
+*/
+const animate = time => {
 	renderer.render(scene, camera);
 	requestAnimationFrame(animate); // Animate loop
 }
 
-animate();
-
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.update();
+requestAnimationFrame(animate);
